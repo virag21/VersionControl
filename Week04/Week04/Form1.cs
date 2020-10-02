c657679d427eef03ef85e059a061e4042b0a551c
Virág Ványi <virag.vanyi@stud.uni-corvinus.hu>
2020-10-02 19:18:36 +0200

    final commit

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -91,11 +91,16 @@ namespace Week04
             int counter = 0;
             foreach (Flat f in Flats)
             {
+                if (f.Elevator== true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else { values[counter, 4] = "Nincs"; }
+
                 values[counter, 0] = f.Code;
                 values[counter, 1] = f.Vendor;
                 values[counter, 2] = f.Side;
                 values[counter, 3] = f.District;
-                values[counter, 4] = f.Elevator;
                 values[counter, 5] = f.NumberOfRooms;
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
