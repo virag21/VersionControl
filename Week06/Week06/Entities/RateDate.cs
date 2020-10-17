1672dd94dfac0b07eb58c82d94e201ddd0e96002
Virág Ványi <virag.vanyi@stud.uni-corvinus.hu>
2020-10-17 22:20:19 +0200

    datagridview and xml

---
+++ Week06/Week06/Entities/RateDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week06.Entities
+{
+    class RateDate
+    {
+        DateTime Date;
+        string Currency;
+        decimal Value;
+    }
+}
--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Week06.Entities;
+using Week06.MnbServiceReference;
 
 namespace Week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateDate> Rates = new BindingList<RateDate>();
         public Form1()
         {
             InitializeComponent();
+            GetRates();
+            XML();
+            dataGridView1.DataSource = Rates;
+
+        }
+
+        
+
+        public static void GetRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
+        private static void XML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
