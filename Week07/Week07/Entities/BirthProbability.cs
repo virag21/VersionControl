0e978ab2dcbcf230f26da331136f05d9e196f897
Virág Ványi <virag.vanyi@stud.uni-corvinus.hu>
2020-10-24 16:03:22 +0200

    classes added

---
+++ Week07/Week07/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week07.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int Children { get; set; }
+        public double BirthP { get; set; }
+    }
+}
---
+++ Week07/Week07/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week07.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double DeathP { get; set; }
+    }
+}
