d2cbd7eeaf2887dc1f3380a311243022752ec98b
Virág Ványi <virag.vanyi@stud.uni-corvinus.hu>
2020-11-15 00:37:19 +0100

    CarFactory added

--- Week08_GXMRLU/Week08_GXMRLU/Entities/CarFactory.cs
+++ Week08_GXMRLU/Week08_GXMRLU/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using Week08_GXMRLU.Abstractions;
 
 namespace Week08_GXMRLU.Entities
 {
-    class BallFactory:IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
